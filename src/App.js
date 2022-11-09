c88aa860a87f27bd29151c997bd38f27d328ac2e
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-11-09 09:21:20 -0500

    Adding styles to Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
 import useLocalStorage from "use-local-storage";
-import './index.css';
-
+import "./index.css";
 
 function App() {
-
-
-
   return (
     <div className="app">
       <div className="login">
@@ -19,6 +15,29 @@ function App() {
             <i className="fab fa-twitter-square"></i>
             <i className="fab fa-apple"></i>
           </div>
+          <p className="divider">
+            <span>Or</span>
+          </p>
+          <form>
+            <label>E-mail</label>
+            <input type="email" placeholder="Enter your Email" />
+            <label>Password</label>
+            <input type="password" placeholder="Enter your Password" />
+            <div className="remember">
+              <input type="checkbox" checked="checked" />
+              <p>Remember me</p>
+            </div>
+            <button>Log in</button>
+          </form>
+          <div className="bottom">
+            <p>Forget your password?</p>
+            <a href="/">Reset Password</a>
+          </div>
+          <p className="create">Create Account</p>
+        </div>
+        <div className="theme-toggle">
+          <h2>Ligth Theme</h2>
+          <i className="fas fa-toggle-on"></i>
         </div>
       </div>
     </div>
